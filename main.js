70ff0e21bdc64218ec0aeac283efb2908afd4de8
unknown <wldudsmile97@gmail.com>
2025-02-24 17:15:16 +0900

    긍정확언 양식 생성

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
  는 여러줄 주석
 */
 //전역변수 (global variable)
-let diary
+let diary =""
 //함수정의부
 function savehandler(){
     // 사용자가 웹페이지에서 입력한 값을 메모리에 저장
@@ -19,18 +19,44 @@ function savehandler(){
     localStorage.setItem("diary",diary)
     let storage1 = localStorage.getItem("diary")
     console.log(storage1)
+    
+    // 예외처리 
+    if (date == "" ) {
+        alert("날짜를 선택하세요.")
+        return false;}
+    if (text == "" ) {
+        alert("긍정확언을 입력하세요.")
+        return false;}
+       
+    
+    
+    
+    $("#date").val("");
+    $("#text").val("");
 }
+
 // 스토리지 테스트
 // localStorage.setItem("test-key", "테스트입니다")
 // let storage1 = localStorage.getItem("test-key")
 // console.log(storage1)
 
 
+// =의 의미는 오른쪽 값을 왼쪽에 저장한다 
+// ==의 의미는 양쪽 비교 
 
 // j쿼리 실행시작 
-$(document).ready(function(){
-    //문서가 로딩되면 로컬
-    diary = localStorage.getItem("diary")
-    //id가 save로 정의한 요소를 클릭했을 때, savehandler 호출
-    $("#save").click(savehandler)
-})
+
+//문서가 로딩되면 로컬
+
+$(document).ready(function() {
+    diary = localStorage.getItem("diary");
+    
+     if (diary == null)  {
+        diary = "";
+     }
+
+
+//id가 save로 정의한 요소를 클릭했을 때, savehandler 호출
+$("#save").click(savehandler)
+
+});
